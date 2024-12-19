5d895a2547ab7c6120242bdcae4bbf550b98cbdb
canolayoo78 <canolayoo78@gmail.com>
2024-12-19 23:44:29 +0900

    [Silver V] Title: 수 정렬하기 2, Time: 468 ms, Memory: 97964 KB -BaekjoonHub

--- 백준/Silver/2751. 수 정렬하기 2/수 정렬하기 2.py
+++ 백준/Silver/2751. 수 정렬하기 2/수 정렬하기 2.py
@@ -1,11 +1,10 @@
-import sys
+def main():
+    import io, os
+    input = io.BufferedReader(io.FileIO(0), buffer_size=1 << 18).readline
+    N = int(input())
+    exist = bytearray(2000001)
+    for _ in range(N):
+        exist[int(input()) + 1000000] = True
+    os.write(1, '\n'.join(str(i) for i, ei in enumerate(exist, -1000000) if ei).encode())
 
-N = int(sys.stdin.readline())
-exist = [None] * 2000001
-
-for _ in range(N):
-    exist[int(sys.stdin.readline()) + 1000000] = 1
-
-for i in range(len(exist)):
-    if exist[i] == 1:
-        print(i - 1000000)
+main()
