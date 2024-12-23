a0bec8fa0164e2942d8abe11cef03168f9349dbf
canolayoo78 <canolayoo78@gmail.com>
2024-12-23 18:20:20 +0900

    [Silver IV] Title: 수 찾기, Time: 148 ms, Memory: 52416 KB -BaekjoonHub

--- 백준/Silver/1920. 수 찾기/수 찾기.py
+++ 백준/Silver/1920. 수 찾기/수 찾기.py
@@ -3,10 +3,10 @@ import sys
 input = sys.stdin.readline
 
 N = int(input())
-z_N = set(map(int, input().split()))
+z_N = set(input().split())
 
 M = int(input())
-z_M = map(int, input().split())
+z_M = input().split()
 
 for key in z_M:
     print(int(key in z_N))
