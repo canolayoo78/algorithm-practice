a5875763f1922d8d98a69b3d9b58b7b2ab986b82
canolayoo78 <canolayoo78@gmail.com>
2024-12-24 21:46:28 +0900

    [Silver IV] Title: 카드2, Time: 128 ms, Memory: 51888 KB -BaekjoonHub

---
+++ 백준/Silver/2164. 카드2/카드2.py
@@ -0,0 +1,17 @@
+import sys
+from collections import deque
+
+input = sys.stdin.readline
+
+
+def find_last_card(N):
+    queue = deque(range(1, N + 1))
+
+    while len(queue) > 1:
+        queue.popleft()
+        queue.append(queue.popleft())
+    return queue[0]
+
+
+N = int(input())
+print(find_last_card(N))
