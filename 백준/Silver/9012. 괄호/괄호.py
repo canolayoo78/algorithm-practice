7967d80e2ded96c6c7b322ea8646c8cafba2027d
canolayoo78 <canolayoo78@gmail.com>
2024-12-25 22:10:41 +0900

    [Silver IV] Title: 괄호, Time: 40 ms, Memory: 32412 KB -BaekjoonHub

--- 백준/Silver/9012. 괄호/괄호.py
+++ 백준/Silver/9012. 괄호/괄호.py
@@ -1,20 +1,16 @@
 import sys
-from collections import deque
 
 input = sys.stdin.readline
 
 
 def is_parenthesis_string(input_str):
-    stack = deque()
-    for i in range(len(input_str)):
-        if input_str[i] == "(":
-            stack.append("(")
-        elif input_str[i] == ")":
-            if stack:
-                stack.pop()
-            else:
-                return "NO"
-    if stack:
+    while True:
+        if "()" in input_str:
+            input_str = input_str.replace("()", "")
+        else:
+            break
+
+    if input_str:
         return "NO"
     else:
         return "YES"
@@ -22,4 +18,4 @@ def is_parenthesis_string(input_str):
 
 T = int(input())
 for _ in range(T):
-    print(is_parenthesis_string(input()))
+    print(is_parenthesis_string(input().strip()))
