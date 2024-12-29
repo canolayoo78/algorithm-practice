b1eddc632f2d4e67d960b1d84d71305d7aad4388
canolayoo78 <canolayoo78@gmail.com>
2024-12-29 20:27:07 +0900

    [Silver III] Title: 피보나치 함수, Time: 36 ms, Memory: 32412 KB -BaekjoonHub

--- 백준/Silver/1003. 피보나치 함수/피보나치 함수.py
+++ 백준/Silver/1003. 피보나치 함수/피보나치 함수.py
@@ -5,17 +5,16 @@ input = sys.stdin.readline
 
 def solution():
     T = int(input())
-
     cases = [int(input()) for _ in range(T)]
+
     max_case = max(cases)
-    zeros = [1, 0]
-    ones = [0, 1]
-    for i in range(2, max_case + 1):
-        zeros.append(zeros[i - 1] + zeros[i - 2])
-        ones.append(ones[i - 1] + ones[i - 2])
+    result = [1, 0]
+
+    for i in range(2, max_case + 2):
+        result.append(result[i - 1] + result[i - 2])
 
     for case in cases:
-        print(f"{zeros[case]} {ones[case]}")
+        print(f"{result[case]} {result[case+1]}")
 
 
 solution()
