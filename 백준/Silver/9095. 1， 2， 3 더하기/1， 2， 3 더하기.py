2d447d550c6f0e962835468b1a303296140940a9
canolayoo78 <canolayoo78@gmail.com>
2024-12-30 14:49:37 +0900

    [Silver III] Title: 1, 2, 3 더하기, Time: 32 ms, Memory: 32412 KB -BaekjoonHub

--- 백준/Silver/9095. 1， 2， 3 더하기/1， 2， 3 더하기.py
+++ 백준/Silver/9095. 1， 2， 3 더하기/1， 2， 3 더하기.py
@@ -2,10 +2,11 @@ import sys
 
 input = sys.stdin.readline
 
+dp = [1, 2, 4]
+
 
 def solution(N):
-    dp = [1, 2, 4]
-    for i in range(3, N):
+    for i in range(len(dp), N):
         dp.append(sum(dp[i - 3 : i]))
     print(dp[N - 1])
 
