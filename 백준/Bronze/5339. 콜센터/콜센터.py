e7e53b468bdbd34ff0c758b43e67e7070d4365eb
canolayoo78 <canolayoo78@gmail.com>
2025-02-15 01:58:31 +0900

    [Bronze V] Title: 콜센터, Time: 32 ms, Memory: 32412 KB -BaekjoonHub

---
+++ 백준/Bronze/5339. 콜센터/콜센터.py
@@ -0,0 +1,19 @@
+import sys
+
+input = sys.stdin.readline
+
+
+def sol():
+    print("     /~\\")
+    print("    ( oo|")
+    print("    _\\=/_")
+    print("   /  _  \\")
+    print("  //|/.\\|\\\\")
+    print(" ||  \\ /  ||")
+    print("============")
+    print("|          |")
+    print("|          |")
+    print("|          |")
+
+
+sol()
