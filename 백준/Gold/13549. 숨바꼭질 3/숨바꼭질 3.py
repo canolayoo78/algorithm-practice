7087cbb2a9b0764355026186a5a4475876b0a1da
canolayoo78 <canolayoo78@gmail.com>
2025-02-26 23:59:35 +0900

    [Gold V] Title: 숨바꼭질 3, Time: 100 ms, Memory: 38368 KB -BaekjoonHub

--- 백준/Gold/13549. 숨바꼭질 3/숨바꼭질 3.py
+++ 백준/Gold/13549. 숨바꼭질 3/숨바꼭질 3.py
@@ -1,22 +1,29 @@
 import sys
 from collections import deque
 
-N, K = map(int, sys.stdin.readline().rstrip().split())
-visited = [-1] * 100001
+MAX = 10**5
+visited = [-1] * (MAX + 1)
+input = sys.stdin.readline
 
-q = deque([N])
-visited[N] = 0
-while q:
-    now = q.popleft()
-    if now == K:
-        print(visited[now])
-        exit()
-    if 0 <= now-1 <= 100000 and visited[now-1] == -1:
-        visited[now-1] = visited[now] + 1
-        q.append(now-1)
-    if 0 <= now*2 <= 100000 and visited[now*2] == -1:
-        visited[now*2] = visited[now] + 0 	# Key Point
-        q.appendleft(now*2) 				# Key Point
-    if 0 <= now+1 <= 100000 and visited[now+1] == -1:
-        visited[now+1] = visited[now] + 1
-        q.append(now+1)
+
+def solution(N, K):
+    q = deque([N])
+    visited[N] = 0
+    while q:
+        now = q.popleft()
+        if now == K:
+            return visited[now]
+        if 0 <= now - 1 <= 100000 and visited[now - 1] == -1:
+            visited[now - 1] = visited[now] + 1
+            q.append(now - 1)
+        if 0 <= now * 2 <= 100000 and visited[now * 2] == -1:
+            visited[now * 2] = visited[now] + 0
+            q.appendleft(now * 2)
+        if 0 <= now + 1 <= 100000 and visited[now + 1] == -1:
+            visited[now + 1] = visited[now] + 1
+            q.append(now + 1)
+
+
+if __name__ == "__main__":
+    N, K = map(int, input().split())
+    print(solution(N, K))
