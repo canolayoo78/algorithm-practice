2dc4672f177aac8cc5b5c9b6c395f11f589b94a2
canolayoo78 <canolayoo78@gmail.com>
2025-03-18 16:47:31 +0900

    [Silver I] Title: 구간 합 구하기 5, Time: 664 ms, Memory: 73444 KB -BaekjoonHub

--- 백준/Silver/11660. 구간 합 구하기 5/구간 합 구하기 5.py
+++ 백준/Silver/11660. 구간 합 구하기 5/구간 합 구하기 5.py
@@ -5,21 +5,18 @@ input = sys.stdin.readline
 
 def solution():
     N, M = map(int, input().split())
-    table = [list(map(int, input().split())) for _ in range(N)]
 
-    dp = [[0] * (N + 1) for _ in range(N + 1)]
+    table = [[0] + list(map(int, input().split())) for _ in range(N)]
+    table = [[0] * (N + 1)] + table
 
     for i in range(1, N + 1):
         for j in range(1, N + 1):
-            dp[i][j] = (
-                dp[i][j - 1] + dp[i - 1][j] - dp[i - 1][j - 1] + table[i - 1][j - 1]
-            )
+            table[i][j] = table[i][j - 1] + table[i - 1][j] - table[i - 1][j - 1] + table[i][j]
+            
 
     for _ in range(M):
-        x1, y1, x2, y2 = map(int, input().split())
-
-        result = dp[x2][y2] - dp[x2][y1 - 1] - dp[x1 - 1][y2] + dp[x1 - 1][y1 - 1]
-        print(result)
+        sx, sy, ex, ey = map(int, input().split())
+        print(table[ex][ey] - table[sx - 1][ey] - table[ex][sy - 1] + table[sx - 1][sy - 1])
 
 
 if __name__ == "__main__":
