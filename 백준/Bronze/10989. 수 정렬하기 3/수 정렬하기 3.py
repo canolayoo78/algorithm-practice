758a606de47d5b5ca9869ced74a81581c3977480
canolayoo78 <canolayoo78@gmail.com>
2025-03-30 23:22:43 +0900

    [Bronze I] Title: 수 정렬하기 3, Time: 4196 ms, Memory: 33432 KB -BaekjoonHub

--- 백준/Bronze/10989. 수 정렬하기 3/수 정렬하기 3.py
+++ 백준/Bronze/10989. 수 정렬하기 3/수 정렬하기 3.py
@@ -1,18 +1,20 @@
 import sys
 
 input = sys.stdin.readline
+print = sys.stdout.write
 MAX = 10001
 
 
 def solution():
     N = int(input())
     count = [0] * MAX
+
     for _ in range(N):
         count[int(input())] += 1
 
     for i in range(MAX):
-        for j in range(count[i]):
-            print(i)
+        for _ in range(count[i]):
+            print(str(i) + "\n")
 
 
 if __name__ == "__main__":
