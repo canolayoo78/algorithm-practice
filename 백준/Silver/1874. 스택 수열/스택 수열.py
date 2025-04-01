c2b850bb200b7eb95312641bf204dd7422f22b49
canolayoo78 <canolayoo78@gmail.com>
2025-04-01 16:44:25 +0900

    [Silver II] Title: 스택 수열, Time: 104 ms, Memory: 41024 KB -BaekjoonHub

---
+++ 백준/Silver/1874. 스택 수열/스택 수열.py
@@ -0,0 +1,30 @@
+import sys
+from collections import deque
+
+input = sys.stdin.readline
+# print = sys.stdout.write
+
+
+def solution():
+    N = int(input())
+    target = [int(input()) for _ in range(N)]
+    queue = deque()
+    index = 0
+    ans = []
+    for i in range(1, N + 1):
+        queue.append(i)
+        ans.append("+")
+
+        while queue and target[index] == queue[-1]:
+            queue.pop()
+            ans.append("-")
+            index += 1
+
+    if queue:
+        print("NO")
+    else:
+        print("\n".join(ans))
+
+
+if __name__ == "__main__":
+    solution()
