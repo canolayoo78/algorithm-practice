48567ae8e012246156bb2c012be1f7bfc1c25a52
canolayoo78 <canolayoo78@gmail.com>
2025-04-01 17:00:43 +0900

    [Silver IV] Title: 주몽, Time: 64 ms, Memory: 35524 KB -BaekjoonHub

---
+++ 백준/Silver/1940. 주몽/주몽.py
@@ -0,0 +1,32 @@
+import sys
+from collections import deque
+
+input = sys.stdin.readline
+print = sys.stdout.write
+
+
+def solution():
+    N = int(input())
+    M = int(input())
+    materials = list(map(int, input().split()))
+    materials.sort()
+
+    one = 0
+    two = N - 1
+    ans = 0
+
+    while one < two:
+        sum = materials[one] + materials[two]
+        if sum == M:
+            ans += 1
+            one += 1
+            two -= 1
+        elif sum < M:
+            one += 1
+        else:
+            two -= 1
+    print(str(ans))
+
+
+if __name__ == "__main__":
+    solution()
