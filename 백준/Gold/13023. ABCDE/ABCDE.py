89521ae6a12ce1dc19812e8e4b2b126b37e2242d
canolayoo78 <canolayoo78@gmail.com>
2025-04-03 21:32:47 +0900

    [Gold V] Title: ABCDE, Time: 684 ms, Memory: 34944 KB -BaekjoonHub

---
+++ 백준/Gold/13023. ABCDE/ABCDE.py
@@ -0,0 +1,41 @@
+import sys
+from collections import deque
+
+input = sys.stdin.readline
+
+N, M = map(int, input().split())
+graph = [[] for _ in range(N)]
+for _ in range(M):
+    h1, h2 = map(int, input().split())
+    graph[h1].append(h2)
+    graph[h2].append(h1)
+visited = [False] * N
+arrive = False
+
+
+def dfs(now, depth):
+    global arrive
+    if depth == 5 or arrive is True:
+        arrive = True
+        return
+    visited[now] = True
+    for i in graph[now]:
+        if not visited[i]:
+            dfs(i, depth + 1)
+    visited[now] = False
+
+
+def solution():
+    global arrive, visited
+    for i in range(N):
+        dfs(i, 1)
+        if arrive:
+            break
+    if arrive:
+        print(1)
+    else:
+        print(0)
+
+
+if __name__ == "__main__":
+    solution()
