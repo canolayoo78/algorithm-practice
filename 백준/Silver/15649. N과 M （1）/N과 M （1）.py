d8df09bb2ca346e14c9e64fc35da364500cbdcbc
canolayoo78 <canolayoo78@gmail.com>
2025-04-04 17:02:35 +0900

    [Silver III] Title: N과 M (1), Time: 148 ms, Memory: 32412 KB -BaekjoonHub

--- 백준/Silver/15649. N과 M （1）/N과 M （1）.py
+++ 백준/Silver/15649. N과 M （1）/N과 M （1）.py
@@ -1,21 +1,24 @@
 import sys
 
 input = sys.stdin.readline
-N, M = map(int, input().split())
-s = []
 
 
-def dfs():
-    if len(s) == M:
-        print(" ".join(str(number) for number in s))
-        return
+def solution():
+    N, M = map(int, input().split())
+    stack = []
 
-    for i in range(1, N + 1):
-        if i not in s:
-            s.append(i)
-            dfs()
-            s.pop()
+    def dfs():
+        if len(stack) == M:
+            print(" ".join(str(s) for s in stack))
+            return
+        for i in range(1, N + 1):
+            if i not in stack:
+                stack.append(i)
+                dfs()
+                stack.pop()
+
+    dfs()
 
 
 if __name__ == "__main__":
-    dfs()
+    solution()
