c52fcb371067e371c4d4c3a6e5a2553b7cfeda3a
canolayoo78 <canolayoo78@gmail.com>
2025-04-16 18:28:35 +0900

    [Gold V] Title: 신기한 소수, Time: 36 ms, Memory: 32412 KB -BaekjoonHub

--- 백준/Gold/2023. 신기한 소수/신기한 소수.py
+++ 백준/Gold/2023. 신기한 소수/신기한 소수.py
@@ -1,31 +1,35 @@
 import sys
+
 sys.setrecursionlimit(10000)
+input = sys.stdin.readline
+print = sys.stdout.write
 
-N = int(input())
 
-def prime(i):
-    for x in range(2, int((i**(1/2)))+1):
-        if i % x == 0:
-            return False
-    return True
-    
-def DFS(i):
-    if len(str(i)) == N:
-        print(i)
-    else:
-        for x in range(1, 10, 2):
-            if prime(i*10+x):
-                DFS(i*10+x)
-                   
+def solution():
+    N = int(input())
 
-DFS(2)
-DFS(3)
-DFS(5)
-DFS(7)
+    def is_prime(num):
+        for i in range(2, int((num ** (1 / 2))) + 1):
+            if num % i == 0:
+                return False
 
-    
+        return True
 
-        
-    
+    def make_number(number):
+        if len(str(number)) == N:
+            print(str(number) + "\n")
 
-        
+        else:
+            for i in range(1, 10, 2):
+                next_number = number * 10 + i
+                if is_prime(next_number):
+                    make_number(next_number)
+
+    make_number(2)
+    make_number(3)
+    make_number(5)
+    make_number(7)
+
+
+if __name__ == "__main__":
+    solution()
