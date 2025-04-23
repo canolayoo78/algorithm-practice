d081e366b7fd844ed58bf2bca227dbdd821adce1
canolayoo78 <canolayoo78@gmail.com>
2025-04-23 16:55:23 +0900

    [Gold II] Title: 칵테일, Time: 36 ms, Memory: 34536 KB -BaekjoonHub

---
+++ 백준/Gold/1033. 칵테일/칵테일.py
@@ -0,0 +1,49 @@
+import math
+import sys
+
+sys.setrecursionlimit(10000)
+
+input = sys.stdin.readline
+print = sys.stdout.write
+
+
+def solution():
+    N = int(input())
+    graph = [[] for _ in range(N)]
+    visit = [False] * N
+    ingredients = [0] * N
+    lcm = 1
+
+    def gcd(a, b):
+        if b == 0:
+            return a
+        else:
+            return gcd(b, a % b)
+
+    def dfs(start):
+        visit[start] = True
+        for i in graph[start]:
+            next = i[0]
+            if not visit[next]:
+                ingredients[next] = ingredients[start] * i[2] // i[1]
+                dfs(next)
+
+    for _ in range(N - 1):
+        a, b, p, q = map(int, input().split())
+        graph[a].append((b, p, q))
+        graph[b].append((a, q, p))
+        lcm *= p * q // gcd(p, q)
+
+    ingredients[0] = lcm
+    dfs(0)
+
+    ingredients_gcd = lcm
+    for ingredient in ingredients:
+        ingredients_gcd = gcd(ingredients_gcd, ingredient)
+
+    for ingredient in ingredients:
+        print(str(ingredient // ingredients_gcd) + " ")
+
+
+if __name__ == "__main__":
+    solution()
