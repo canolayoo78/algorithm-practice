7a40beb9ef1ada88e11d04bc24a9a2443bc616f8
canolayoo78 <canolayoo78@gmail.com>
2025-04-25 17:29:43 +0900

    [Silver III] Title: 소수 구하기, Time: 120 ms, Memory: 43124 KB -BaekjoonHub

--- 백준/Silver/1929. 소수 구하기/소수 구하기.py
+++ 백준/Silver/1929. 소수 구하기/소수 구하기.py
@@ -1,6 +1,6 @@
 import sys
 
-sys.setrecursionlimit(10000)
+# sys.setrecursionlimit(10000)
 
 input = sys.stdin.readline
 print = sys.stdout.write
@@ -8,17 +8,24 @@ print = sys.stdout.write
 
 def solution():
     M, N = map(int, input().split())
-    numbers = [True] * (N + 1)
-    numbers[1] = False
+    i_limit = (N + 1) // 2
+    check = [False] * i_limit
+    primes = []
 
-    for i in range(2, int(N ** 0.5) + 1):
-        if numbers[i]:
-            for j in range(2 * i, N + 1, i):
-                numbers[j] = False
+    if M <= 2:
+        primes.append(2)
 
-    for i in range(M, N + 1):
-        if numbers[i]:
-            print(str(i) + "\n")
+    for i in range(1, i_limit):
+        if check[i]:
+            continue
+        p = 2 * i + 1
+        if M <= p:
+            primes.append(p)
+
+        for j in range(i, i_limit, p):
+            check[j] = True
+
+    print("\n".join(str(prime) for prime in primes))
 
 
 if __name__ == "__main__":
