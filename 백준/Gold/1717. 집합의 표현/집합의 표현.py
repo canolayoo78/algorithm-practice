a1ea18c2a171dc914629c51df07f8073cb554847
canolayoo78 <canolayoo78@gmail.com>
2025-05-13 17:41:14 +0900

    [Gold V] Title: 집합의 표현, Time: 460 ms, Memory: 124652 KB -BaekjoonHub

---
+++ 백준/Gold/1717. 집합의 표현/집합의 표현.py
@@ -0,0 +1,37 @@
+import sys
+
+input = sys.stdin.readline
+# print = sys.stdout.write
+
+
+def solution():
+    N, M = map(int, input().split())
+    set_list = [i for i in range(N + 1)]
+
+    def find(n):
+        if set_list[n] == n:
+            return n
+        else:
+            set_list[n] = find(set_list[n])
+            return set_list[n]
+
+    def union(a, b):
+        a_rep = find(a)
+        b_rep = find(b)
+
+        if a_rep != b_rep:
+            set_list[b_rep] = a_rep
+
+    for _ in range(M):
+        func, a, b = map(int, input().split())
+        if func == 0:
+            union(a, b)
+        else:
+            if find(a) == find(b):
+                print("yes")
+            else:
+                print("no")
+
+
+if __name__ == "__main__":
+    solution()
