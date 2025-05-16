548f3a117a0044824505e35a00a18c51190c00fd
canolayoo78 <canolayoo78@gmail.com>
2025-05-16 16:44:40 +0900

    [Gold III] Title: 줄 세우기, Time: 148 ms, Memory: 41448 KB -BaekjoonHub

---
+++ 백준/Gold/2252. 줄 세우기/줄 세우기.py
@@ -0,0 +1,35 @@
+import sys
+from collections import deque
+
+input = sys.stdin.readline
+
+
+def solution():
+    N, M = map(int, input().split())
+    graph = [[] for _ in range(N + 1)]
+    in_degree = [0] * (N + 1)
+    for _ in range(M):
+        a, b = map(int, input().split())
+        graph[a].append(b)
+        in_degree[b] += 1
+
+    ans = []
+    q = deque([])
+
+    for i in range(1, N + 1):
+        if in_degree[i] == 0:
+            q.append(i)
+
+    while q:
+        now = q.popleft()
+        ans.append(now)
+        for edge in graph[now]:
+            in_degree[edge] -= 1
+            if in_degree[edge] == 0:
+                q.append(edge)
+
+    print(" ".join(str(a) for a in ans))
+
+
+if __name__ == "__main__":
+    solution()
