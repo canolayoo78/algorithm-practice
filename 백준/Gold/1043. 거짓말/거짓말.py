62abf1a0a6368febf1445925b1abc3a6d6f39c75
canolayoo78 <canolayoo78@gmail.com>
2025-05-16 15:22:24 +0900

    [Gold IV] Title: 거짓말, Time: 40 ms, Memory: 32412 KB -BaekjoonHub

--- 백준/Gold/1043. 거짓말/거짓말.py
+++ 백준/Gold/1043. 거짓말/거짓말.py
@@ -4,11 +4,6 @@ input = sys.stdin.readline
 
 
 def solution():
-    N, M = map(int, input().split())
-    true_people = list(map(int, input().split()))[1:]
-    set_list = [i for i in range(N + 1)]
-    party = [[] for _ in range(M)]
-
     def find(n):
         if set_list[n] == n:
             return n
@@ -23,22 +18,24 @@ def solution():
         if a_rep != b_rep:
             set_list[b_rep] = a_rep
 
-    for i in range(M):
-        party[i] = list(map(int, input().split()))[1:]
-        p_rep = party[i][0]
-        for p in party[i][1:]:
-            union(p_rep, p)
+    N, M = map(int, input().split())
+    set_list = [i for i in range(N + 1)]
+    true_people = list(map(int, input().split()))
+    parties = [list(map(int, input().split())) for _ in range(M)]
 
+    for i in range(1, true_people[0]):
+        union(true_people[i], true_people[i + 1])
+
+    for party in parties:
+        for i in range(1, party[0]):
+            union(party[i], party[i + 1])
+
+    true_set = set_list[true_people[-1]]
     ans = 0
-
-    for i in range(M):
-        is_possible = True
-        p_rep = party[i][0]
-        for true_person in true_people:
-            if find(p_rep) == find(true_person):
-                is_possible = False
-                break
-        if is_possible:
+    for party in parties:
+        if find(party[1]) == find(true_set):
+            union(party[1], true_set)
+        else:
             ans += 1
 
     print(ans)
