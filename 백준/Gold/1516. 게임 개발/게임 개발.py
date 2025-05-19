10d3a92840e2796085749281ae8c82e643e1f4b6
canolayoo78 <canolayoo78@gmail.com>
2025-05-19 15:47:55 +0900

    [Gold III] Title: 게임 개발, Time: 44 ms, Memory: 33432 KB -BaekjoonHub

--- 백준/Gold/1516. 게임 개발/게임 개발.py
+++ 백준/Gold/1516. 게임 개발/게임 개발.py
@@ -1,36 +1,34 @@
 import sys
-from collections import deque
 
-input = sys.stdin.readline
+n = int(sys.stdin.readline())
+build = [[0]]
+for _ in range(n):
+    li = list(map(int, sys.stdin.readline().split()))
+    build.append(li[:-1])
+dp = [-1] * (n + 1)
 
-
-def solution():
-    N = int(input())
-    graph = [[0]]
-    dp = [-1] * (N + 1)
-    for i in range(1, N + 1):
-        build = list(map(int, input().split()))[:-1]
-        graph.append(build)
-
-    def check(idx):
-        if dp[idx] != -1:
-            return dp[idx]
-        
-        if len(graph[idx]) == 1:
-            dp[idx] = graph[idx][0]
-            return dp[idx]
-        
-        prev_time = 0
-        for prev in graph[idx][1:]:
-            prev_time = max(prev_time, check(prev))
-            
-        dp[idx] = prev_time + graph[idx][0]
+def check(idx):
+    if dp[idx] != -1:
         return dp[idx]
-    
-    
 
-    print("\n".join(str(check(i)) for i in range(1, N + 1)))
+    # 아직 방문하지 않은 건물
+    ## 선행 건물 없다면
+    if len(build[idx]) == 1:
+        dp[idx] = build[idx][0]
+        return dp[idx]
+
+    ## 선행 건물 있는 경우
+    ## 건물 i는 가장 늦게 끝나는 선행 건물이 끝나야 지을 수 있다
+    prev_time = 0
+    for prev in build[idx][1:]:
+        prev_time = max(prev_time, check(prev))
+
+    dp[idx] = prev_time + build[idx][0]
+    return dp[idx]
 
 
-if __name__ == "__main__":
-    solution()
+for i in range(1, n + 1):
+    print(check(i))
+
+
+
