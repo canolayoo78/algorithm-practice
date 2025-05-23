1e31341eac1af4b1bad54b45387f02b934935275
canolayoo78 <canolayoo78@gmail.com>
2025-05-23 14:41:05 +0900

    [Gold V] Title: 최소비용 구하기, Time: 144 ms, Memory: 116232 KB -BaekjoonHub

--- 백준/Gold/1916. 최소비용 구하기/최소비용 구하기.py
+++ 백준/Gold/1916. 최소비용 구하기/최소비용 구하기.py
@@ -10,22 +10,30 @@ def solution():
     M = int(input())
     graph = [[] for _ in range(N + 1)]
     distance = [INF] * (N + 1)
+    visited = [0] * (N + 1)
     for _ in range(M):
         u, v, w = map(int, input().split())
         graph[u].append((w, v))
     start, end = map(int, input().split())
-    q = []
-    heapq.heappush(q, (0, start))
+
     distance[start] = 0
 
-    while q:
-        d, now = heapq.heappop(q)
-        if distance[now] < d:
-            continue
-        for d, next in graph[now]:
-            if distance[next] > distance[now] + d:
-                distance[next] = distance[now] + d
-                heapq.heappush(q, (distance[now] + d, next))
+    for _ in range(N):
+        min_index = -1
+        min_dist = INF
+        for i in range(1, N + 1):
+            if not visited[i] and distance[i] < min_dist:
+                min_dist = distance[i]
+                min_index = i
+
+        if min_index == -1:
+            break
+
+        visited[min_index] = 1
+
+        for d, next in graph[min_index]:
+            if not visited[next] and distance[next] > distance[min_index] + d:
+                distance[next] = distance[min_index] + d
 
     print(distance[end])
 
