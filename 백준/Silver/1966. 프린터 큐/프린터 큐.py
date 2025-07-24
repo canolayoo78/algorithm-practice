3bb9ba64b54fa8889dedcba1b0dfedaa09615c7a
canolayoo78 <canolayoo78@gmail.com>
2025-07-24 16:10:50 +0900

    [Silver III] Title: 프린터 큐, Time: 56 ms, Memory: 34924 KB -BaekjoonHub

--- 백준/Silver/1966. 프린터 큐/프린터 큐.py
+++ 백준/Silver/1966. 프린터 큐/프린터 큐.py
@@ -10,28 +10,24 @@ def solution():
     while T > 0:
         N, M = map(int, input().split())
         documents = list(map(int, input().split()))
-        importance = [0] * (10)
         out_stream = deque()
 
         for i in range(N):
             out_stream.append((i, documents[i]))
-            importance[documents[i]] += 1
 
+        importance = deque(sorted(documents, reverse=True))
         order = 1
         while out_stream:
             now_index, now_importance = out_stream.popleft()
-            flag = True
-            for i in range(9, now_importance, -1):
-                if importance[i] != 0:
-                    flag = False
-                    break
-            if flag:
+
+            if now_importance == importance[0]:
                 if now_index == M:
                     print(order)
                     break
                 else:
                     order += 1
-                    importance[now_importance] -= 1
+                    importance.popleft()
+
             else:
                 out_stream.append((now_index, now_importance))
 
