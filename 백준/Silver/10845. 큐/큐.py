71a4936cdc670cddc29390a1d7e22dac061bc67e
canolayoo78 <canolayoo78@gmail.com>
2025-08-21 15:31:22 +0900

    [Silver IV] Title: 큐, Time: 64 ms, Memory: 34924 KB -BaekjoonHub

---
+++ 백준/Silver/10845. 큐/큐.py
@@ -0,0 +1,31 @@
+import sys
+from collections import deque
+
+input = sys.stdin.readline
+write = sys.stdout.write
+read = sys.stdin.buffer.read
+sys.setrecursionlimit(10**6)
+
+
+def solution():
+    N = int(input())
+    q = deque([])
+    for _ in range(N):
+        command = input().strip()
+        if command == "pop":
+            print(-1 if not q else q.popleft())
+        elif command == "size":
+            print(len(q))
+        elif command == "empty":
+            print(1 if not q else 0)
+        elif command == "front":
+            print(-1 if not q else q[0])
+        elif command == "back":
+            print(-1 if not q else q[-1])
+        else:
+            _, n = command.split()
+            q.append(int(n))
+
+
+if __name__ == "__main__":
+    solution()
