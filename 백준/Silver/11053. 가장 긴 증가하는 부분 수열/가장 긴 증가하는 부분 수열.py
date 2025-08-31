f70d69ecce1d39c1c896909b544c2bf74a7fa40b
canolayoo78 <canolayoo78@gmail.com>
2025-08-31 16:11:46 +0900

    [Silver II] Title: 가장 긴 증가하는 부분 수열, Time: 96 ms, Memory: 109544 KB -BaekjoonHub

--- 백준/Silver/11053. 가장 긴 증가하는 부분 수열/가장 긴 증가하는 부분 수열.py
+++ 백준/Silver/11053. 가장 긴 증가하는 부분 수열/가장 긴 증가하는 부분 수열.py
@@ -1,19 +1,22 @@
+import bisect
 import sys
 
 input = sys.stdin.readline
 
-N = int(input())
-numbers = list(map(int, input().split()))
-dp = [1] * N
-
 
 def solution():
-    for i in range(1, N):
-        for j in range(i):
-            if numbers[i] > numbers[j]:
-                dp[i] = max(dp[i], dp[j] + 1)
+    N = int(input())
+    num = list(map(int, input().split()))
+    tails = []
 
-    print(max(dp))
+    for i in range(N):
+        pos = bisect.bisect_left(tails, num[i])
+        if pos == len(tails):
+            tails.append(num[i])
+        else:
+            tails[pos] = num[i]
+
+    print(len(tails))
 
 
 if __name__ == "__main__":
