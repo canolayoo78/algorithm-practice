64c974d2514f4a05de8443d24e1e75a809480f67
canolayoo78 <canolayoo78@gmail.com>
2025-09-30 16:51:01 +0900

    [Gold V] Title: LCS, Time: 176 ms, Memory: 32412 KB -BaekjoonHub

--- 백준/Gold/9251. LCS/LCS.py
+++ 백준/Gold/9251. LCS/LCS.py
@@ -6,16 +6,18 @@ input = sys.stdin.readline
 def solution():
     str1 = input().strip()
     str2 = input().strip()
-    dp = [[0] * (len(str2) + 1) for _ in range(len(str1) + 1)]
-
+    prev = [0] * (len(str2) + 1)
+    cur = [0] * (len(str2) + 1)
+    
     for i in range(1, len(str1) + 1):
         for j in range(1, len(str2) + 1):
             if str1[i - 1] == str2[j - 1]:
-                dp[i][j] = dp[i - 1][j - 1] + 1
+                cur[j] = prev[j - 1] + 1
             else:
-                dp[i][j] = max(dp[i - 1][j], dp[i][j - 1])
-
-    print(dp[-1][-1])
+                cur[j] = max(prev[j], cur[j - 1])
+        prev = cur.copy()
+      
+    print(cur[-1])
 
 
 if __name__ == "__main__":
